a7057f28c87a9a0be6ac8c8283890bdc496bf4c2
Wemimo Oyedeji <wemmyoyedeji@gmail.com>
2022-01-31 03:02:31 +0000

    add text title test to button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { shallow } from "enzyme";
+
 import Button from "./index";
 
+const wrapper = shallow(<Button title="submit" />);
+
 it("renders without crashing", () => {
-  expect(shallow(<Button title="submit" />)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
+});
+it("renders correct button title", () => {
+  expect(wrapper.text()).toEqual("submit");
 });
